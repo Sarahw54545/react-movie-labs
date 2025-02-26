146a2a817978ea0dafc05be34ad6ee3c9d496fa4
Sarah Walsh <20102911@mail.wit.ie>
2025-02-26 23:46:26 +0000

    Added Upcoming Movies Page

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes} from "react-router";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
@@ -15,7 +16,7 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 360000,
-      refetchInterval: 360000, 
+      refetchInterval: 360000,
       refetchOnWindowFocus: false
     },
   },
@@ -28,12 +29,13 @@ const App = () => {
         <SiteHeader />
         <MoviesContextProvider>
           <Routes>
-            <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-            <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-            <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
-            <Route path="*" element={ <Navigate to="/" /> } />
-            <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
+            <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+            <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+            <Route path="/reviews/:id" element={<MovieReviewPage />} />
+            <Route path="/movies/:id" element={<MoviePage />} />
+            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
           </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
---
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from '@tanstack/react-query';
+import Spinner from '../components/spinner';
+
+const UpcomingMoviesPage = (props) => {
+
+  const { data, error, isPending, isError  } = useQuery({
+    queryKey: ['discoverUpcomingMovies'],
+    queryFn: getUpcomingMovies,
+  })
+  
+  if (isPending) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  
+  const movies = data.results;
+
+  return (
+    <PageTemplate
+      title="Upcoming Movies"
+      movies={movies}
+      action={() => {}}
+    />
+);
+};
+export default UpcomingMoviesPage;

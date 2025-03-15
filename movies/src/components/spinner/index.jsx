8ae152c1a591a68451bfaa3d898847f9e662225c
Sarah Walsh <20102911@mail.wit.ie>
2025-03-15 22:36:43 +0000

    Clean Up Before Changes

--- movies/src/components/spinner/index.jsx
+++ movies/src/components/spinner/index.jsx
@@ -3,13 +3,17 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 export default function CircularIndeterminate() {
   return (
-    <div sx={{
+    <div 
+      sx= {
+        {
         display: 'flex',
         justifyContent: "center",
         '& > * + *': {
-          marginLeft: '2em',
-        }}}>
-      <CircularProgress />
+        marginLeft: '2em',
+        }
+      }
+      }>
+
       <CircularProgress />
     </div>
   );
